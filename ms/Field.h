9cc409e07f0b380ec71473a8755f113c9debd537
UNIDY2002 <UNIDY2002@outlook.com>
2022-01-15 20:40:21 +0800

    添加一些注释

--- ms/Field.h
+++ ms/Field.h
@@ -4,28 +4,28 @@
 #include <vector>
 
 struct Field {
-    std::string name;
+    std::string name;                          // 字段名
     enum Type {
         STR,
         INT,
         FLOAT,
-    } type;
-    std::size_t str_len;
-    bool nullable;
-    bool has_def;
-    int def_int;
-    float def_float;
-    std::string def_str;
+    } type;                                    // 字段类型
+    std::size_t str_len;                       // 字符串类型的最大长度
+    bool nullable;                             // 字段是否可空
+    bool has_def;                              // 字段是否有默认值
+    int def_int;                               // INT 型默认值
+    float def_float;                           // FLOAT 型默认值
+    std::string def_str;                       // STR 型默认值
 };
 
 struct PrimaryField {
-    std::string name;
-    std::vector<std::string> columns;
+    std::string name;                          // 约束名称
+    std::vector<std::string> columns;          // 约束各列名称
 };
 
 struct ForeignField {
-    std::string name;
-    std::vector<std::string> columns;
-    std::string foreign_table_name;
-    std::vector<std::string> foreign_columns;
+    std::string name;                          // 约束名称
+    std::vector<std::string> columns;          // 从表各列名称
+    std::string foreign_table_name;            // 主表名称
+    std::vector<std::string> foreign_columns;  // 主表各列名称
 };
